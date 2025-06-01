be6f83165bb701e836bdfff379aa85269ee2b8cf
Amnan <amnanbudikusuma@gmail.com>
2025-06-02 03:11:53 +0700

    refactor: rename class to Main and move to 'app' package

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,10 +1,9 @@
-package project_prak_pbo;
+package app;
 
 import controller.DashboardController;
 import view.DashboardView;
 
-public class Project_prak_pbo {
-
+public class Main {
     public static void main(String[] args) {
         javax.swing.SwingUtilities.invokeLater( () -> {
             DashboardView view = new DashboardView();
