2d639fc6942481293f111b2f27def3dc35c2bd3c
Amnan <amnanbudikusuma@gmail.com>
2025-06-02 03:26:05 +0700

    refactor: extract listener initialization into separate methods in DashboardController

--- src/controller/DashboardController.java
+++ src/controller/DashboardController.java
@@ -3,42 +3,51 @@ package controller;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import view.DashboardView;
 import view.AddReservationView;
-import view.ShowReservationView;
+import view.DashboardView;
 import view.RoomDetailsView;
+import view.ShowReservationView;
 
 public class DashboardController {
     private DashboardView view;
     
     public DashboardController(DashboardView view) {
         this.view = view;
-        
         initController();
     }
-    
+
     private void initController() {
+        initAddReservationListener();
+        initShowReservationListener();
+        initRoomDetailsListener();
+    }
+
+    private void initAddReservationListener() {
         view.setBtnAddReservationListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                AddReservationView addView = new AddReservationView();
-                new AddReservationController(addView);
+                AddReservationView addReservationView = new AddReservationView();
+                new AddReservationController(addReservationView);
                 view.dispose();
             }
         });
-        
+    }
+
+    private void initShowReservationListener() {
         view.setBtnShowReservationListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                ShowReservationView showView = new ShowReservationView();
-                new ShowReservationController(showView);
+                ShowReservationView showReservationView = new ShowReservationView();
+                new ShowReservationController(showReservationView);
                 view.dispose();
             }
         });
-        
+    }
+
+    private void initRoomDetailsListener() {
         view.setBtnRoomDetailsListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                RoomDetailsView roomView = new RoomDetailsView();
-                new RoomDetailsController(roomView);
+                RoomDetailsView roomDetailsView = new RoomDetailsView();
+                new RoomDetailsController(roomDetailsView);
                 view.dispose();
             }
         });
